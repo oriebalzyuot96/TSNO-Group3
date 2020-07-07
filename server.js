468c7182dff2460449e8a87f8668953246511f4d
oriebalzyuot96 <=>
2020-07-07 11:45:47 +0300

    build select input axsios in hundle submit and search function in server update

--- server.js
+++ server.js
@@ -29,22 +29,25 @@ mongoose
     .then(() => console.log('DataBase connected to the server'))
     .catch(err => console.log(err))
 
+     
 var Users = require('./routes/Users')
-var Search= require('./routes/search')
+//var Search= require('./routes/search')
 
 app.use('/users', Users)
-app.use('/search',Search)
+///app.use('/search',Search)
 
-// app.get("/search", function(req, res)  {
-//     var toSearch= req.body
-//     console.log(toSearch)
-//      Users.find(toSearch,function(err, data)  {
-//          if(err){
-//              throw err;
-//          }
-//          res.json(data);
-//      });
-//  });
+
+const User = require('./models/User');
+app.get("/search", function(req, res)  {
+    var toSearch= req.body
+    console.log(toSearch)
+     User.find(toSearch,function(err, data)  {
+         if(err){
+             throw err;
+         }
+         res.json(data);
+     });
+ });
 
 
 
